484229f40ebc109cc8dbd7e6cbe773b489f21ed8
Willem Renders <willem.renders@student.uantwerpen.be>
2017-05-03 13:26:24 +0200

    mapping-works

--- pyd7a-master/examples/Fingerpriting-Mapping.py
+++ pyd7a-master/examples/Fingerpriting-Mapping.py
@@ -1,7 +1,22 @@
 from sys import argv
 import paho.mqtt.client as mqtt
 import json
+import os
 prompt = '> '
+
+
+#b5700000912bf
+gateway1 = []
+#b5700000912fd
+gateway2 = []
+#b5700000912d9
+gateway3 = []
+#b570000091ac9
+gateway4 = []
+#b5700000912d5
+gateway5 = []
+#b570000091291
+gateway6 = []
 
 def on_connect(mqttc, obj, flags, rc):
     print("rc: "+str(rc))
@@ -16,7 +31,21 @@ def on_message(mqttc, obj, msg):
 
     if(payload_JSON["node"]==nodeid):
         print payload_JSON
-        f.write(str(payload_JSON))
+        RSSI_value = payload_JSON["link_budget"]
+        gatewayID = str(payload_JSON["gateway"])
+
+        if gatewayID == 'b5700000912bf':
+            gateway1.append(RSSI_value)
+        if gatewayID == 'b5700000912fd':
+            gateway2.append(RSSI_value)
+        if gatewayID == 'b5700000912d9':
+            gateway3.append(RSSI_value)
+        if gatewayID == 'b570000091ac9':
+            gateway4.append(RSSI_value)
+        if gatewayID == 'b5700000912d5':
+            gateway5.append(RSSI_value)
+        if gatewayID == 'b570000091291':
+            gateway6.append(RSSI_value)        
         global count
         count = count + 1
 
@@ -52,11 +81,24 @@ print("Topic: " + MQTT_topic)
 print
 
 # CREATE JSON FILE
-print "Creating new JSON file..."
-print "Give location name:"
+print "Writing JSON file..."
+print "Give file name:"
 file_name = raw_input(prompt)
 file_path = "../Mapping/"
-f = open( file_path + file_name + ".JSON", 'a')
+file_path = file_path + file_name + ".JSON"
+f = open( file_path, 'a')
+if os.stat(file_path).st_size != 0:
+    f.seek(-1,os.SEEK_END)
+    f.truncate()
+    f.write(",")
+else:
+    f.write("[")
+
+# CREATE ROOM
+print "Give room name:"
+room_name = raw_input(prompt)
+print "Give room id:"
+room_id = raw_input(prompt)
 
 #Create connection
 print("Create connection....")
@@ -64,9 +106,41 @@ mqttc.connect(MQTT_server, 1883, 60)
 mqttc.subscribe(MQTT_topic)
 print("Connection created....\n")
 
-
 count = 0
-while count < 20:
+while count < 60:
     mqttc.loop()
 
+if len(gateway1) > 0:
+    RSSI_gateway1 = round(sum(gateway1) / float(len(gateway1)))
+if len(gateway2) > 0:
+    RSSI_gateway2 = round(sum(gateway2) / float(len(gateway2)))
+if len(gateway3) > 0:
+    RSSI_gateway3 = round(sum(gateway3) / float(len(gateway3)))
+if len(gateway4) > 0:
+    RSSI_gateway4 = round(sum(gateway4) / float(len(gateway4)))
+if len(gateway5) > 0:
+    RSSI_gateway5 = round(sum(gateway5) / float(len(gateway5)))
+if len(gateway6) > 0:
+    RSSI_gateway6 = round(sum(gateway6) / float(len(gateway6)))
+
+data = {}
+data['room_id'] = room_id
+data['room_name'] = room_name
+RSSI = []
+if len(gateway1) > 0:
+    RSSI.append({"Gateway": "gateway1","RSSI-Value": RSSI_gateway1 })
+if len(gateway2) > 0:
+    RSSI.append({"Gateway": "gateway2","RSSI-Value": RSSI_gateway2 })
+if len(gateway3) > 0:
+    RSSI.append({"Gateway": "gateway3","RSSI-Value": RSSI_gateway3 })
+if len(gateway4) > 0:
+    RSSI.append({"Gateway": "gateway4","RSSI-Value": RSSI_gateway4 })
+if len(gateway5) > 0:
+    RSSI.append({"Gateway": "gateway5","RSSI-Value": RSSI_gateway5 })
+if len(gateway6) > 0:
+    RSSI.append({"Gateway": "gateway6","RSSI-Value": RSSI_gateway6 })
+data['RSSI'] = RSSI
+json_data = json.dumps(data)
+f.write(json_data)
+f.write("]")
 f.close()
