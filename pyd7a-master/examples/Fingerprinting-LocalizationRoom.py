d3088f7c43f8dbdad8e4b4b193b64745de3dc387
Willem Renders <willem.renders@student.uantwerpen.be>
2017-05-03 14:27:32 +0200

    LocalRoom

--- pyd7a-master/examples/Fingerprinting-LocalizationRoom.py
+++ pyd7a-master/examples/Fingerprinting-LocalizationRoom.py
@@ -1,4 +1,35 @@
 from sys import argv
 import paho.mqtt.client as mqtt
 import json
+import math
+import operator 
 
+def euclideanDistance(instance1, instance2, length):
+	distance = 0
+	for x in range(length):
+		distance += pow((instance1[x] - instance2[x]), 2)
+	return math.sqrt(distance)
+
+def getNeighbors(trainingSet, testInstance, k):
+	distances = []
+	length = len(testInstance)-1
+	for x in range(len(trainingSet)):
+		dist = euclideanDistance(testInstance, trainingSet[x], length)
+		distances.append((trainingSet[x], dist))
+	distances.sort(key=operator.itemgetter(1))
+	neighbors = []
+	for x in range(k):
+		neighbors.append(distances[x][0])
+	return neighbors
+
+def getTrainset():
+trainSet = [[2, 2, 2, 'V315','1'], [4, 4, 4, 'V315','2']]
+
+def gettestInstance():
+testInstance = [5, 5, 5]
+
+k = 1
+neighbors = getNeighbors(trainSet, testInstance, 1)
+print "You are in"
+print "Room: " + neighbors[0][3]
+print "ID: " + neighbors[0][4]
