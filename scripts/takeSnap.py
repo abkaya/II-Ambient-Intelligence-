bcae462e7afefc42fa82335e170ac320d237f4da
abkaya <abdil.kaya@student.uantwerpen.be>
2017-05-18 22:16:46 +0200

    amend file change to last commit (takeSnap.py)

--- scripts/takeSnap.py
+++ scripts/takeSnap.py
@@ -8,14 +8,14 @@ camera = PiCamera()
 camera.rotation = 0
 camera.start_preview()
 
-bashCommand="sudo convert cameratemp.jpg -pointsize 24 -fill yellow -annotate +270+460 %[exif:DateTimeOriginal] cameratemp1.jpg"
+bashCommand='sudo convert cameratemp.jpg -pointsize 24 -fill #00CCFF -annotate +380+50 %[exif:DateTimeOriginal] cameratemp.jpg'
 
 #camera.resolution = (640, 480)
 while True:
   camera.capture('cameratemp.jpg')
   process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
   output, error = process.communicate()
-  os.rename("cameratemp1.jpg", "camera.jpg")
+  os.rename("cameratemp.jpg", "camera.jpg")
   sleep(4)
 
 camera.stop_preview()
